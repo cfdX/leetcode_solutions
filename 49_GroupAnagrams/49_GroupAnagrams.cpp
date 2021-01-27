ae75875897d69fbd8b6adc7b56708d3f7c4fcb70
Daniil Savchenko <cfdx@list.ru>
2021-01-27 21:03:15 +0300

    49 Group Anagrams implementation (using unordered_map)

--- 49_GroupAnagrams/49_GroupAnagrams.cpp
+++ 49_GroupAnagrams/49_GroupAnagrams.cpp
@@ -1,35 +1,36 @@
 #include "49_GroupAnagrams.h"
 
 #include <algorithm>
+#include <unordered_map>
 
 namespace lc::t49 {
+
+namespace {
+using VecS = std::vector<std::string>;
+} // namespace {
 
 //------------------------------
 [[nodiscard]]
 auto Solution::groupAnagrams(
             std::vector<std::string> const& strs )
         -> std::vector<std::vector<std::string>> {
-    auto sorted_base = std::vector<std::string>{};
-    auto res = std::vector<std::vector<std::string>>{};
+    auto map = std::unordered_map<std::string, VecS>{};
+    // оптимизация, чтоб буфер переиспользовать
     auto sorted_str = std::string{};
     for ( const auto& str: strs ) {
         sorted_str = str;
         std::sort( sorted_str.begin(), sorted_str.end() );
-        const auto iter = std::lower_bound(
-              sorted_base.begin()
-            , sorted_base.end()
-            , sorted_str
-        );
-        const auto dist = std::distance(
-              sorted_base.begin()
-            , iter
-        );
-        if ( ( iter == sorted_base.end() ) || (*iter != sorted_str) ) {
-            sorted_base.insert( iter, sorted_str );
-            res.insert( res.begin() + dist, { str } );
+        auto iter = map.find( sorted_str );
+        if ( iter == map.end() ) {
+            map.insert( { sorted_str, { str } } );
         } else {
-            res[ dist ].push_back( str );
+            iter->second.push_back( str );
         }
+    }
+    auto res = std::vector<VecS>{};
+    res.reserve( map.size() );
+    for ( auto&& pair : map ) {
+        res.push_back( std::move( pair.second ) );
     }
     return res;
 }
