e8f7094258f74f20de3e3c54ff8363c3db9ba289
Daniil Savchenko <cfdx@list.ru>
2021-02-01 20:43:57 +0300

    37 Sudoku Solver - cleaning and optimization solution

--- 37_SudokuSolver/37_SudokuSolver.cpp
+++ 37_SudokuSolver/37_SudokuSolver.cpp
@@ -4,8 +4,6 @@
 #include <bitset>
 #include <optional>
 
-#include <iostream>
-
 namespace lc::t37 {
 
 namespace {
@@ -17,50 +15,59 @@ constexpr const auto kBoardSize = kBoxCount*kBoxCount;
 //------------------------------
 using CharBoard = std::vector<std::vector<char>>;
 
+using Num = unsigned int;
+constexpr const auto kNoValue = static_cast<Num>( kBoardSize );
+
 //------------------------------
 template<class T, std::size_t kSize = kBoardSize>
 using Matr = std::array<std::array<T, kSize>, kSize>;
 
 // Битами показываем, какие возможны варианты в ячейке
-using CellDecisions = std::bitset<kBoardSize>;
-
-using Decisions = Matr<CellDecisions>;
-
-std::string step;
-struct D {
-    D() {
-        step += "  ";
-    }
-    ~D() {
-        step.resize( step.size() - 2 );
-    }
-};
+using Variants = std::bitset<kBoardSize>;
 
 //------------------------------
 [[nodiscard]]
-auto toNum( char const c ) noexcept -> CellDecisions {
+constexpr
+auto toNum( char const c ) noexcept -> Num {
     if ( (c >= '1') && (c <= '9') ) {
-        return CellDecisions{}.set( static_cast<std::size_t>( c - '1' ), true );
+        return static_cast<Num>( c - '1' );
     }
-    return CellDecisions{};
+    return kNoValue;
 }
 
 //------------------------------
 // предполагается, что на входе сет только с одним битом
 // TODO это очень дебильный способ узнать индекс установленного бита
-// но мне лень сейчас с битами возиться, а эта не эффективность
-// особо не должна влиять на общее время работы алгоритмаы
+// но мне лень сейчас с битами возиться, и это лучше чем цикл c test[i]
 [[nodiscard]]
-auto fromNum( CellDecisions const& d ) noexcept -> char {
-    if ( d.count() != 1 ) { // TODO!!!!!!!!!!!!!!!!!!!! Это не нужно тогда
-        return '.';
+auto numFromSet( Variants const& d ) noexcept -> Num {
+    static_assert( kBoardSize == 9, "this switch is designed for 9*9 board" );
+    switch ( d.to_ullong() ) {
+        case 1: return 0;
+        case 2: return 1;
+        case 4: return 2;
+        case 8: return 3;
+        case 16: return 4;
+        case 32: return 5;
+        case 64: return 6;
+        case 128: return 7;
+        case 256: return 8;
+        default: {}
     }
-    for ( std::size_t i = 0; i < 9; ++i ) {
-        if ( d.test( i ) == true ) {
-            return '1' + static_cast<char>(i);
-        }
-    }
-    return '.';
+    return kNoValue;
+}
+
+//------------------------------
+[[nodiscard]]
+constexpr
+auto toChar( Num const d ) noexcept -> char {
+    return static_cast<char>( d ) + '1';
+}
+
+//------------------------------
+[[nodiscard]]
+auto numToSet( Num n ) noexcept -> Variants {
+    return Variants{}.set( n, true );
 }
 
 //------------------------------
@@ -77,12 +84,28 @@ struct CellInd {
 
 //------------------------------
 [[nodiscard]]
-auto boxForCell( std::size_t i, std::size_t j ) noexcept -> BoxInd {
-    return { i / kBoxCount, j / kBoxCount };
+constexpr
+auto boxForRow( std::size_t i ) noexcept -> std::size_t {
+    return i / kBoxCount;
 }
 
 //------------------------------
 [[nodiscard]]
+constexpr
+auto boxForCol( std::size_t i ) noexcept -> std::size_t {
+    return boxForRow( i );
+}
+
+//------------------------------
+[[nodiscard]]
+constexpr
+auto boxForCell( std::size_t i, std::size_t j ) noexcept -> BoxInd {
+    return { boxForRow(i), boxForCol(j) };
+}
+
+//------------------------------
+[[nodiscard]]
+constexpr
 auto cellInBox( BoxInd bi, std::size_t i, std::size_t j ) noexcept -> CellInd {
     return { bi.box_i*kBoxCount + i, bi.box_j*kBoxCount + j };
 }
@@ -91,135 +114,103 @@ auto cellInBox( BoxInd bi, std::size_t i, std::size_t j ) noexcept -> CellInd {
 class DecisionDesc {
 public:
     //------------------------------
-    DecisionDesc() {
+    DecisionDesc() noexcept {
         fillInit();
     }
+    //------------------------------
     DecisionDesc( CharBoard const& board ) noexcept {
         fillInit();
         m_unsolved_count = kBoardSize*kBoardSize;
         for ( std::size_t i = 0; i < kBoardSize; ++i ) {
+            const auto box_ri = boxForRow( i );
             for ( std::size_t j = 0; j < kBoardSize; ++j ) {
                 const auto val = toNum( board[i][j] );
-                if ( val.count() == 1 ) {
+                if ( val == kNoValue ) {
+                    m_decisions[i][j] = kNoValue;
+                } else {
                     --m_unsolved_count;
                     m_decisions[i][j] = val;
-                    m_solved[i][j] = true;
-                } else {
-                    m_decisions[i][j] = CellDecisions{}.flip();
+                    const auto mask = numToSet( val ).flip();
+                    m_rows_unused[ i ] &= mask;
+                    m_cols_unused[ j ] &= mask;
+                    m_boxes_unused[ box_ri ][ boxForCol(j) ] &= mask;
                 }
-
             }
         }
         tryAutoSolveAll();
-    }
-
-    void print() const {
-        for ( std::size_t i = 0; i < kBoardSize; ++i ) {
-            std::cout << step;
-            for ( std::size_t j = 0; j < kBoardSize; ++j ) {
-                std::cout << fromNum( variants( i, j ) ) ;
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-
-        for ( std::size_t i = 0; i < kBoardSize; ++i ) {
-            std::cout << step;
-            for ( std::size_t j = 0; j < kBoardSize; ++j ) {
-                std::cout << variants( i, j ).to_string() << ' ';
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
     }
 
     //------------------------------
     void fillBoard( CharBoard& board ) const noexcept {
         for ( std::size_t i = 0; i < kBoardSize; ++i ) {
             for ( std::size_t j = 0; j < kBoardSize; ++j ) {
-                board[i][j] = fromNum( variants( i, j ) );
+                board[i][j] = toChar( m_decisions[i][j] );
             }
         }
     }
     //------------------------------
     [[nodiscard]]
-    auto variants( std::size_t i, std::size_t j ) const noexcept -> CellDecisions {
-        return m_decisions[i][j];
+    auto variants( std::size_t i, std::size_t j, BoxInd bi ) const noexcept -> Variants {
+        return m_rows_unused[i] & m_cols_unused[j] & m_boxes_unused[bi.box_i][bi.box_j];
     }
     //------------------------------
-    // считаем, что vars.count() == 1
-    // то есть это окончательное решение
-    // и более того, оно подходит (не делает решение не валидным)
-    void setVariants( std::size_t i, std::size_t j, CellDecisions vars ) noexcept {
-        if ( true == m_solved[i][j] ) {
+    void setVariants( std::size_t i, std::size_t j, Num val, BoxInd bi ) noexcept {
+        if ( m_decisions[ i ][ j ] == val ) {
             return;
         }
-        m_decisions[ i ][ j ] = vars;
-        m_solved[ i ][ j ] = true;
+        m_decisions[ i ][ j ] = val;
         // поставили пометки
         --m_unsolved_count;
-        const auto box = boxForCell( i, j );
-
-
-
-        const auto mask = ~vars;
-        for ( std::size_t ci = 0; ci < kBoardSize; ++ci ) {
-            if ( ci != j ) {
-                m_decisions[ i ][ ci ] &= mask;
-            }
+        const auto mask = numToSet( val ).flip();
+        m_rows_unused[ i ] &= mask;
+        m_cols_unused[ j ] &= mask;
+        m_boxes_unused[ bi.box_i ][ bi.box_j ] &= mask;
+        if ( true == isSolved() ) {
+            return;
         }
-        for ( std::size_t ri = 0; ri < kBoardSize; ++ri ) {
-            if ( ri != i ) {
-                m_decisions[ ri ][ j ] &= mask;
-            }
-        }
-        const auto bi = boxForCell( i, j );
-        // можно сделать несколько эффективнее за счет проверки индекса перед вторым циклом
-        for ( std::size_t ri = 0; ri < kBoxCount; ++ri ) {
-            for ( std::size_t ci = 0; ci < kBoxCount; ++ci ) {
-                const auto cell = cellInBox( bi, ri, ci );
-                if ( cell == CellInd{ i, j } ) {
-                    continue;
-                }
-                m_decisions[ cell.cell_i ][ cell.cell_j ] &= mask;
-            }
-        }
-
-        tryAutoSolveRow( i );
-        tryAutoSolveCol( j );
-        tryAutoSolveBox( box );
-
+        tryAutoSolveRow( i, bi.box_i );
+        tryAutoSolveCol( j, bi.box_j );
+        tryAutoSolveBox( bi );
     }
-    // проверяет, можно ли поставить в ячейку значение
-    // предполагаем, что это, конечное, решение (то есть стоит лишь один бит)
+    //------------------------------
     [[nodiscard]]
-    auto isApplyable( std::size_t i, std::size_t j, CellDecisions vars ) const noexcept -> bool {
-        if ( m_solved[i][j] == true ) {
+    auto isApplyable( std::size_t i, std::size_t j, Num val, BoxInd bi ) const noexcept -> bool {
+        if ( m_decisions[i][j] != kNoValue ) {
             return false;
         }
-        if ( (variants( i, j ) & vars).count() == 0 ) {
+        if ( false == variants( i, j, bi )[val] ) {
             return false;
         }
-        const auto mask = ~vars;
+        const auto mask = numToSet( val ).flip();
         for ( std::size_t ci = 0; ci < kBoardSize; ++ci ) {
-            if ( (ci != j) && ( (variants( i, ci ) & mask).count() == 0 ) ) {
+            if ( m_decisions[i][ci] != kNoValue ) {
+                continue;
+            }
+            const auto box = BoxInd{ bi.box_i, boxForCol( ci ) };
+            if ( (ci != j) && ( (variants( i, ci, box ) & mask).count() == 0 ) ) {
                 return false;
             }
         }
         for ( std::size_t ri = 0; ri < kBoardSize; ++ri ) {
-            if ( (ri != i) && ( (variants( ri, j ) & mask).count() == 0 ) ) {
+            if ( m_decisions[ri][j] != kNoValue ) {
+                continue;
+            }
+            const auto box = BoxInd{ boxForRow( ri ), bi.box_j };
+            if ( (ri != i) && ( (variants( ri, j, box ) & mask).count() == 0 ) ) {
                 return false;
             }
         }
-        const auto bi = boxForCell( i, j );
         // можно сделать несколько эффективнее за счет проверки индекса перед вторым циклом
         for ( std::size_t ri = 0; ri < kBoxCount; ++ri ) {
             for ( std::size_t ci = 0; ci < kBoxCount; ++ci ) {
                 const auto cell = cellInBox( bi, ri, ci );
+                if ( m_decisions[cell.cell_i][cell.cell_j] != kNoValue ) {
+                    continue;
+                }
                 if ( cell == CellInd{ i, j } ) {
                     continue;
                 }
-                if ( ( variants( cell.cell_i, cell.cell_j ) & mask).count() == 0 ) {
+                if ( ( variants( cell.cell_i, cell.cell_j, bi ) & mask).count() == 0 ) {
                     return false;
                 }
             }
@@ -230,9 +221,9 @@ public:
     // предполагается, что ячейка ветвления не была
     // ячейкой с принятым решением и переданный вариант валиден
     [[nodiscard]]
-    auto forkTheWorld( std::size_t i, std::size_t j, std::size_t num ) const noexcept  -> DecisionDesc {
+    auto forkTheWorld( std::size_t i, std::size_t j, Num num, BoxInd bi ) const noexcept  -> DecisionDesc {
         auto copy = *this;
-        copy.setVariants( i, j, CellDecisions{}.set( num, true ) );
+        copy.setVariants( i, j, num, bi );
         return copy;
     }
     //------------------------------
@@ -244,15 +235,18 @@ public:
     [[nodiscard]]
     auto findBetterForForkCell() const noexcept -> std::optional<CellInd> {
         auto res = std::optional<CellInd>{};
-        // это важно, variants( 0,  ).count() может не подойти
-        // если в 0, 0 уже принято решение
         auto min_cnt = kBoardSize + 1;
         for ( std::size_t ri = 0; ri < kBoardSize; ++ri ) {
+            if ( m_rows_unused[ri].count() == 0 ) {
+                continue;
+            }
+            const auto box_ri = boxForRow( ri );
             for ( std::size_t ci = 0; ci < kBoardSize; ++ci ) {
-                const auto cnt = variants( ri, ci ).count();
-                if ( cnt < 2 ) {
+                if ( m_decisions[ri][ci] != kNoValue ) {
                     continue;
                 }
+                const auto bi = BoxInd{ box_ri, boxForCol( ci ) };
+                const auto cnt = variants( ri, ci, bi ).count();
                 if ( cnt <= min_cnt ) {
                     min_cnt = cnt;
                     res = CellInd{ ri, ci };
@@ -263,85 +257,113 @@ public:
     }
 private:
     //------------------------------
-    void fillInit() {
-        auto arr9 = std::array<CellDecisions,kBoardSize>{};
-        arr9.fill( CellDecisions{}.flip() );
-        m_decisions.fill( arr9 );
+    void fillInit() noexcept {
+        const auto val = Variants{}.flip();
+        auto arr9 = std::array<Variants,kBoardSize>{};
+        arr9.fill( val );
+        m_rows_unused = arr9;
+        m_cols_unused = arr9;
+        auto arr3 = std::array<Variants,kBoxCount>{};
+        arr3.fill( val );
+        m_boxes_unused.fill( arr3 );
     }
 public:
     //------------------------------
-    void tryAutoSolveRow( std::size_t ri ) noexcept {
-        if ( true == isSolved() ) {
-            return;
-        }
+    void tryAutoSolveRow( std::size_t ri, std::size_t box_row ) noexcept {
         for ( std::size_t ci = 0; ci < kBoardSize; ++ci ) {
-            if ( variants( ri, ci ).count() == 1 ) {
-                setVariants( ri, ci, variants( ri, ci ) );
+            if ( m_decisions[ri][ci] != kNoValue ) {
+                continue;
+            }
+            const auto bi = BoxInd{ box_row, boxForCol( ci ) };
+            const auto vars = variants( ri, ci, bi );
+            if ( vars.count() == 1 ) {
+                setVariants( ri, ci, numFromSet( vars ), bi );
             }
         }
     }
-    void tryAutoSolveCol( std::size_t ci ) noexcept {
-        if ( true == isSolved() ) {
-            return;
-        }
+    //------------------------------
+    void tryAutoSolveCol( std::size_t ci, std::size_t box_col ) noexcept {
         for ( std::size_t ri = 0; ri < kBoardSize; ++ri ) {
-            if ( variants( ri, ci ).count() == 1 ) {
-                setVariants( ri, ci, variants( ri, ci ) );
+            if ( m_decisions[ri][ci] != kNoValue ) {
+                continue;
+            }
+            const auto bi = BoxInd{ boxForRow( ri ), box_col };
+            const auto vars = variants( ri, ci, bi );
+            if ( vars.count() == 1 ) {
+                setVariants( ri, ci, numFromSet( vars ), bi );
             }
         }
     }
+    //------------------------------
     void tryAutoSolveBox( BoxInd bi ) noexcept {
-        if ( true == isSolved() ) {
-            return;
-        }
         for ( std::size_t ri = 0; ri < kBoxCount; ++ri ) {
             for ( std::size_t ci = 0; ci < kBoxCount; ++ci ) {
                 const auto cell = cellInBox( bi, ri, ci );
-                if ( variants( cell.cell_i, cell.cell_j ).count() == 1 ) {
-                    setVariants( cell.cell_i, cell.cell_j, variants( cell.cell_i, cell.cell_j ) );
+                if ( m_decisions[cell.cell_i][ cell.cell_j] != kNoValue ) {
+                    continue;
+                }
+                const auto vars = variants(
+                    cell.cell_i, cell.cell_j, bi
+                );
+                if ( vars.count() == 1 ) {
+                    setVariants(
+                          cell.cell_i
+                        , cell.cell_j
+                        , numFromSet( vars )
+                        , bi
+                    );
                 }
             }
         }
     }
+    //------------------------------
     // А это штука найдет все варианты
     void tryAutoSolveAll() noexcept {
         if ( true == isSolved() ) {
             return;
         }
         for ( std::size_t ri = 0; ri < kBoardSize; ++ri ) {
-            tryAutoSolveRow( ri );
+            if ( 0 == m_rows_unused[ ri ].count() ) {
+                continue;
+            }
+            tryAutoSolveRow( ri, boxForRow( ri ) );
         }
     }
 private:
     // принятые решения
-    Decisions m_decisions{};
-    Matr<bool> m_solved{};
+    Matr<Num> m_decisions{};
+    // не занятые числа для каждой строки, столбца и клетки
+    std::array<Variants, kBoardSize> m_rows_unused{};
+    std::array<Variants, kBoardSize> m_cols_unused{};
+    Matr<Variants, kBoxCount> m_boxes_unused{};
     // сколько всего клеток, где пока непонятно
     std::size_t m_unsolved_count = 0;
 };
 
 //------------------------------
 [[nodiscard]]
-auto solve( DecisionDesc const& desc ) -> std::optional<DecisionDesc> {
+auto solve( DecisionDesc const& desc ) noexcept -> std::optional<DecisionDesc> {
     if ( true == desc.isSolved() ) {
         return desc;
     }
     const auto mb_best_cell = desc.findBetterForForkCell();
-    // TOTHINK не очень понятно, как мы тогда здесь оказались, мы ведь проверяем isApplyable
     if ( false == mb_best_cell.has_value() ) {
+        // unreachable
+        // не очень понятно, как мы тогда здесь оказались, мы ведь проверяем isApplyable
         return {};
     }
     const auto best_cell = mb_best_cell.value();
-    for ( std::size_t i = 0; i < kBoardSize; ++i ) {
-//        std::cout << step << 'x' << std::endl;
-        const auto des = CellDecisions{}.set( i, true );
-        if ( false == desc.isApplyable( best_cell.cell_i, best_cell.cell_j, des ) ) {
+    const auto box_for_cell = boxForCell( best_cell.cell_i, best_cell.cell_j );
+    for ( Num val = 0; val < static_cast<Num>(kBoardSize); ++val ) {
+        const auto is_applyable = desc.isApplyable(
+            best_cell.cell_i, best_cell.cell_j, val, box_for_cell
+        );
+        if ( false == is_applyable ) {
             continue;
         }
-        auto new_desc = desc.forkTheWorld(
-            best_cell.cell_i, best_cell.cell_j, i
+        const auto new_desc = desc.forkTheWorld(
+            best_cell.cell_i, best_cell.cell_j, val, box_for_cell
         );
-        new_desc.tryAutoSolveAll();
         const auto res = solve( new_desc );
         if ( false == res.has_value() ) {
             continue;
@@ -349,7 +371,7 @@ auto solve( DecisionDesc const& desc ) -> std::optional<DecisionDesc> {
 //        if ( true == res->isSolved() ) {
 //            return res;
 //        }
-        return res; //!!!!!!!!!!!!!!!!
+        return res; // мы не проверяем решение на полноту, так как иначе бы его не вернули бы
     }
     return {};
 }
